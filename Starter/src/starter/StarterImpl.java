36f56513fc8d1d6be9f3587867304c6be6a1aac4
Jannik-S <jannik.schick@gmail.com>
2014-04-23 11:46:59 +0200

    starterImpl extends

--- Starter/src/starter/StarterImpl.java
+++ Starter/src/starter/StarterImpl.java
@@ -1,5 +1,25 @@
 package starter;
 
-public class StarterImpl {
+import ggt.StarterPackage.exInvalidCount;
+
+public class StarterImpl extends ggt.StarterPOA {
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createProcess(int count) throws exInvalidCount {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void quit() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
